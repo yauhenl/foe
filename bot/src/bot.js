a6c6bdc45533841e7869f7b99d182a54f2fd1a92
yauhenl <yauhen.lisimau@gmail.com>
2017-10-27 18:03:49 +0300

    Initial commit

---
+++ bot/src/bot.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         FoE Bot
+// @namespace    http://tampermonkey.net/empty.html
+// @version      0.1
+// @description  FoE Bot
+// @author       yauhenl
+// @run-at       document-end
+// @include      /^(http|https)://[a-z]+[0-9]+\.forgeofempires\.com/game/.*/
+// @connect      /^[a-z]+[0-9]+\.forgeofempires\.com/
+// @connect      innogamescdn.com
+// @grant        none
+// @match        http://tampermonkey.net/empty.html
+// @require      file:///C:/Users/yauhenl/IdeaProjects/foe/bot/src/bot.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("test")
+})();
